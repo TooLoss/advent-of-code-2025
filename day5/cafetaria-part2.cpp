c1f9d7e8294cc615806f23b94ed0afc1ac980972
Bilèle <bilele.elhaddadi@gmail.com>
2025-12-06 17:38:18 +0100

    day 5 part 2 works but slow

--- day5/cafetaria-part2.cpp
+++ day5/cafetaria-part2.cpp
@@ -5,11 +5,11 @@
 #include <math.h>
 
 
-#define ULONGI long long int 
+#define ULONGI long long int
 
 
 void parse_dash(std::string pair, ULONGI &begin, ULONGI &end) {
-    int separator_pos = pair.find('-');
+    size_t separator_pos = pair.find('-');
     if (separator_pos != -1) {
         begin = std::stoll(pair.substr(0, separator_pos));
         end = std::stoll(pair.substr(separator_pos+1));
@@ -22,7 +22,6 @@ void parse_dash(std::string pair, ULONGI &begin, ULONGI &end) {
 
 void make_database(std::ifstream &file, std::vector<std::pair<ULONGI, ULONGI>> &database) {
     std::string line;
-
     while (getline(file, line)) {
         if (line == "") break;
         
@@ -35,7 +34,7 @@ void make_database(std::ifstream &file, std::vector<std::pair<ULONGI, ULONGI>> &
 
 
 ULONGI card(std::pair<ULONGI, ULONGI> A) {
-    return A.second - A.first;
+    return A.second - A.first + 1;
 }
 
 
@@ -46,19 +45,19 @@ std::pair<ULONGI, ULONGI> intersection(std::pair<ULONGI, ULONGI> A, std::pair<UL
 }
 
 
-std::pair<ULONGI, ULONGI> intersection_range(const std::vector<std::pair<ULONGI, ULONGI>> &database, int begin, int end) {
+std::pair<ULONGI, ULONGI> intersection_range(const std::vector<std::pair<ULONGI, ULONGI>> &database, size_t begin, size_t end) {
+    std::pair<ULONGI, ULONGI> inter = database[begin];
     if (begin > end || begin < 0 || end >= database.size()) {
         return {-1, -1};
     }
-    std::pair<ULONGI, ULONGI> inter = database[begin];
-    for (int i = begin + 1; i <= end; i++) {
+    for (size_t i = begin + 1; i <= end; i++) {
         inter = intersection(inter, database[i]);
     }
     return inter;
 }
 
 
-std::vector<std::vector<size_t>> get_all_segment(size_t n) {
+std::vector<std::vector<size_t>> get_all_subset(size_t n) {
     std::vector<std::vector<size_t>> subsets;
     for (size_t mask = 1; mask < (1 << n); mask++) {
         std::vector<size_t> subset;
@@ -75,37 +74,32 @@ std::vector<std::vector<size_t>> get_all_segment(size_t n) {
 
 ULONGI card_union_database(std::vector<std::pair<ULONGI, ULONGI>> &database) {
     ULONGI card_total = 0;
-    auto all_segment = get_all_segment(database.size() - 1);
-    for (const auto I : all_segment) {
-        const auto intersection = database[I[0]];
-        for (const auto value : I) {
-            intersection = intersection(intersection, database[]);
-            ULONGI card_current = 0;
-
-            if (inter_current.first != static_cast<ULONGI>(-1) && inter_current.second != static_cast<ULONGI>(-1))
-                card_current = inter_current.second - inter_current.first;
-
-            int sign = (pair.second - pair.first) % 2 == 0 ? 1 : -1;
-            card_total += sign * card_current;
+    auto all_subset = get_all_subset(database.size());
+    for (const auto &subset : all_subset) {
+        std::vector<std::pair<ULONGI, ULONGI>> subset_intervals;
+        for (size_t index : subset) {
+            subset_intervals.push_back(database[index]);
         }
+        auto inter_current = intersection_range(subset_intervals, 0, subset_intervals.size() - 1);
+        ULONGI card_current = 0;
+        if (inter_current.first != static_cast<ULONGI>(-1) && inter_current.second != static_cast<ULONGI>(-1)) {
+            card_current = inter_current.second - inter_current.first + 1; // Include endpoints
+        }
+        int sign = (subset.size() % 2 == 1) ? 1 : -1;
+        card_total += sign * card_current;
     }
     return card_total;
 }
 
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) return 1;
-
+    if (argc != 2) return 0;
     std::string filename = argv[1];
     std::ifstream file{filename};
-
-    if (!file.good()) return 1;
-
+    if (!file.good()) return 0;
     std::vector<std::pair<ULONGI, ULONGI>> database;
     make_database(file, database);
     ULONGI count = card_union_database(database);
-
     std::cout << count << std::endl;  
-
-    return 0;
+    return 1;
 }
